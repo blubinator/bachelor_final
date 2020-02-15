ca88a9e06ceaca06c179242cc1964788e8bf3a04
tim.reicheneder <tim.reicheneder@stp-online.de>
2020-02-15 17:58:14 +0100

    prepro roi

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -2,10 +2,11 @@ import cv2
 import numpy as np
 from PIL import Image
 import pytesseract
+import imutils
 
 # resize
 def resize(img):
-    dsize = (1600, 1000)
+    dsize = (1920, 1080)
     resized_img = cv2.resize(img, dsize)
     return resized_img
 
@@ -20,7 +21,7 @@ def detect_card(path):
     img = cv2.imread(path)
     img = resize(img)
 
-    show("original", img)
+    #show("original", img)
 
     # img variants
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -29,16 +30,16 @@ def detect_card(path):
 
     edged = cv2.Canny(blur_gray, 30, 150)
 
-    show("edged", edged)
+    #show("edged", edged)
 
     edged_blur = cv2.GaussianBlur(edged,(3, 3),cv2.BORDER_DEFAULT)
 
-    show("edged_blur", edged_blur)
+    #show("edged_blur", edged_blur)
 
     # contours and rectangle detection
     (cnts, hier) = cv2.findContours(edged_blur, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     cv2.drawContours(img, cnts, -1, (0, 255, 0), 2)
-    show("contours", img)
+    #show("contours", img)
     screenCnt = None
     area = 0
     area_index = 0
@@ -65,12 +66,12 @@ def detect_card(path):
     # homography
     pts_src = np.array(screenCnt)
 
-    pts_dst = np.array([[1600, 1000], [1600, 0], [0, 0], [0, 1000]])
+    pts_dst = np.array([[1920, 1080], [1920, 0], [0, 0], [0, 1080]])
 
     h, status = cv2.findHomography(pts_src, pts_dst)
         
 
-    im_out = cv2.warpPerspective(img, h, (1600, 1000))
+    im_out = cv2.warpPerspective(img, h, (1920, 1080))
         
     show("Warped Source Image", im_out)
     return im_out
@@ -78,9 +79,28 @@ def detect_card(path):
 def extract_roi():
     print("asd")
 
-if __name__ == "__main__":
-    img = detect_card("C:\\Users\\tim.reicheneder\\Desktop\Bachelorthesis\\impl_final\\pictures\\fuehrerschein0.jpg")
+def perform_ocr(img):
+    blur = cv2.GaussianBlur(img,(3, 3),cv2.BORDER_DEFAULT)
 
-    print(pytesseract.image_to_string(img))
+    pil_img = Image.fromarray(blur, 'RGB')
+    print(pytesseract.image_to_string(pil_img, lang = 'deu'))
+    data = pytesseract.image_to_data(pil_img, output_type=pytesseract.Output.DICT)
+    h = pil_img.height
+    w = pil_img.width
+    n_boxes = len(data['level'])
+    for i in range(n_boxes):
+        (x, y, w, h) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    show("ayy", img)
+
+
+if __name__ == "__main__":
+    ####### set tesseract path manually ########
+    pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+    ############################################
+
+    img = detect_card("C:\\Users\\tim.reicheneder\\Desktop\Bachelorthesis\\impl_final\\pictures\\fuehrerschein0.jpg")
+    perform_ocr(img)
    
 
